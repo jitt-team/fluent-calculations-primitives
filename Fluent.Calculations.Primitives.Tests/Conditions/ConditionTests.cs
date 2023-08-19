fe198a3aba7a5b7916f0ded0c11e7d9bd26b8b98
Edgars Pivovarenoks <edgarsp@jitt.lv>
2023-08-19 13:19:12 +0300

    tests improvements

--- Fluent.Calculations.Primitives.Tests/Conditions/ConditionTests.cs
+++ Fluent.Calculations.Primitives.Tests/Conditions/ConditionTests.cs
@@ -10,37 +10,44 @@ public class ConditionTests
     public void should_be_false() => Condition.False().IsTrue.Should().BeFalse();
 
     [Fact]
-    public void greater_than_should_work()
+    public void greater_with_left_greater_should_be_true()
     {
         Condition result = Number.Of(10) > Number.Of(1);
         result.IsTrue.Should().BeTrue();
     }
 
     [Fact]
-    public void less_than_should_work()
+    public void lesser_with_left_greater_should_be_false()
     {
         Condition result = Number.Of(10) < Number.Of(1);
         result.IsTrue.Should().BeFalse();
     }
 
     [Fact]
-    public void equal_should_work()
+    public void equalling_non_equal_should_be_false()
     {
-        var result = Number.Of(10) == Number.Of(1);
-
-        result.Should().BeOfType<Condition>();
-
-        ((bool)result).Should().BeFalse();
+        Condition result = Number.Of(10) == Number.Of(1);
+        result.IsTrue.Should().BeFalse();
     }
 
+    [Fact]
+    public void equalling_equal_should_be_true()
+    {
+        Condition result = Number.Of(10) == Number.Of(10);
+        result.IsTrue.Should().BeTrue();
+    }
 
     [Fact]
-    public void not_equal_should_work()
+    public void nonEqualling_not_equal_should_be_true()
     {
-        var result = Number.Of(10) != Number.Of(1);
+        Condition result = Number.Of(10) != Number.Of(1);
+        result.IsTrue.Should().BeTrue();
+    }
 
-        result.Should().BeOfType<Condition>();
-
-        ((bool)result).Should().BeTrue();
+    [Fact]
+    public void nonEqualling_equal_should_be_false()
+    {
+        Condition result = Number.Of(10) != Number.Of(10);
+        result.IsTrue.Should().BeFalse();
     }
 }
