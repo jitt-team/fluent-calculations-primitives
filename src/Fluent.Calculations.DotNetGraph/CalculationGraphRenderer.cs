5b99bd4c8e2187e1a3d5d73c9abc7afbc0315a4b
Edgars Pivovarenoks <edgarsp@jitt.lv>
2023-08-30 19:32:27 +0300

    formatting

--- src/Fluent.Calculations.DotNetGraph/CalculationGraphRenderer.cs
+++ src/Fluent.Calculations.DotNetGraph/CalculationGraphRenderer.cs
@@ -2,68 +2,66 @@
 using DotNetGraph.Core;
 using DotNetGraph.Extensions;
 using Fluent.Calculations.Primitives.BaseTypes;
+namespace Fluent.Calculations.DotNetGraph;
 
-namespace Fluent.Calculations.DotNetGraph
+public class CalculationGraphRenderer
 {
-    public class CalculationGraphRenderer
+    private readonly string graphFileName;
+
+    public CalculationGraphRenderer(string graphFileName)
     {
-        private readonly string graphFileName;
+        this.graphFileName = graphFileName;
+    }
 
-        public CalculationGraphRenderer(string graphFileName)
+    public async Task Render(IValue value)
+    {
+        DotGraph graph = new DotGraph().WithIdentifier("FooGraph").Directed();
+        ToNode(value, graph);
+        await SaveToDot(graph);
+    }
+
+    private DotNode ToNode(IValue value, DotGraph graph)
+    {
+        var parent = new DotNode()
+             .WithIdentifier(System.Web.HttpUtility.HtmlEncode(value.Name))
+             .WithShape(DotNodeShape.Rectangle)
+             .WithLabel(ToHtmlNode(
+                     System.Web.HttpUtility.HtmlEncode($"{value.Name}"),
+                     System.Web.HttpUtility.HtmlEncode($"{value.Expression.Body}"),
+                     System.Web.HttpUtility.HtmlEncode($"{value.PrimitiveValue:0.00}")),
+                     isHtml: true);
+
+        graph.Add(parent);
+        foreach (IValue item in value.Expression.Arguments)
         {
-            this.graphFileName = graphFileName;
+            var child = ToNode(item, graph);
+            var edge = new DotEdge().From(child).To(parent);
+            graph.Add(child);
+            graph.Add(edge);
         }
 
-        public async Task Render(IValue value)
-        {
-            DotGraph graph = new DotGraph().WithIdentifier("FooGraph").Directed();
-            ToNode(value, graph);
-            await SaveToDot(graph);
-        }
+        return parent;
+    }
 
-        private DotNode ToNode(IValue value, DotGraph graph)
-        {
-            var parent = new DotNode()
-                 .WithIdentifier(System.Web.HttpUtility.HtmlEncode(value.Name))
-                 .WithShape(DotNodeShape.Rectangle)
-                 .WithLabel(ToHtmlNode(
-                         System.Web.HttpUtility.HtmlEncode($"{value.Name}"),
-                         System.Web.HttpUtility.HtmlEncode($"{value.Expression.Body}"),
-                         System.Web.HttpUtility.HtmlEncode($"{value.PrimitiveValue:0.00}")),
-                         isHtml: true);
-
-            graph.Add(parent);
-            foreach (IValue item in value.Expression.Arguments)
-            {
-                var child = ToNode(item, graph);
-                var edge = new DotEdge().From(child).To(parent);
-                graph.Add(child);
-                graph.Add(edge);
-            }
-
-            return parent;
-        }
-
-        private string ToHtmlNode(string name, string expression, string value)
-        {
-            return $@"<table border=""0"">
+    private string ToHtmlNode(string name, string expression, string value)
+    {
+        return $@"<table border=""0"">
                     <tr><td align=""left""><b>{name}</b></td></tr>
                     <tr><td align=""left"">{expression}</td></tr>
                     <tr><td align=""left"">{value}</td></tr>
                 </table>";
 
-        }
+    }
 
-        private async Task SaveToDot(DotGraph graph)
-        {
-            await using var writer = new StringWriter();
-            var context = new CompilationContext(writer, new CompilationOptions());
-            await graph.CompileAsync(context);
+    private async Task SaveToDot(DotGraph graph)
+    {
+        await using var writer = new StringWriter();
+        var context = new CompilationContext(writer, new CompilationOptions());
+        await graph.CompileAsync(context);
 
-            var result = writer.GetStringBuilder().ToString();
+        var result = writer.GetStringBuilder().ToString();
 
-            // Save it to a file
-            File.WriteAllText(graphFileName, result);
-        }
+        // Save it to a file
+        File.WriteAllText(graphFileName, result);
     }
 }
