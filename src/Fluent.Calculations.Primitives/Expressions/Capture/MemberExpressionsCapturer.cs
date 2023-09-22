25468397c74b912c0db30cead6a24ebcd3e834ed
Edgars Pivovarenoks <edgarsp@jitt.lv>
2023-09-22 21:25:46 +0300

    cleaner code

--- src/Fluent.Calculations.Primitives/Expressions/Capture/MemberExpressionsCapturer.cs
+++ src/Fluent.Calculations.Primitives/Expressions/Capture/MemberExpressionsCapturer.cs
@@ -6,20 +6,24 @@ using System.Linq.Expressions;
 
 public class MemberExpressionsCapturer : ExpressionVisitor, IMemberExpressionsCapturer
 {
-    private readonly List<MemberExpression> memberExpressions = new List<MemberExpression>();
+    private readonly List<MemberExpression> capturedMemberExpressions = new List<MemberExpression>();
 
     public List<MemberExpression> Capture<TExpressionResulValue>(Expression<Func<TExpressionResulValue>> lamdaExpression) where TExpressionResulValue : class, IValue
     {
         Visit(lamdaExpression);
-        return memberExpressions;
+
+        return capturedMemberExpressions;
     }
 
     protected override Expression VisitMember(MemberExpression node) => base.VisitMember(CaptureIfNotExists(node));
 
     private MemberExpression CaptureIfNotExists(MemberExpression node)
     {
-        if (!memberExpressions.Any(e => e.Member.Name.Equals(node.Member.Name)))
-            memberExpressions.Add(node);
+        if (!capturedMemberExpressions.Any(WithSameName))
+            capturedMemberExpressions.Add(node);
+
         return node;
+
+        bool WithSameName(MemberExpression memberExpression) => memberExpression.Member.Name.Equals(node.Member.Name))
     }
 }
