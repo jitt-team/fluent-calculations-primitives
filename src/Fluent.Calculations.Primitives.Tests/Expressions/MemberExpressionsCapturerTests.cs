17c4e743cc6ea76c986efa0e58cf3920d343d7d4
Edgars Pivovarenoks <edgarsp@jitt.lv>
2023-09-23 13:30:06 +0300

    visitor micro optimization

---
+++ src/Fluent.Calculations.Primitives.Tests/Expressions/MemberExpressionsCapturerTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fluent.Calculations.Primitives.Tests.Expressions
+{
+    internal class MemberExpressionsCapturerTests
+    {
+    }
+}
--- src/Fluent.Calculations.Primitives/Expressions/Capture/MemberExpressionsCapturer.cs
+++ src/Fluent.Calculations.Primitives/Expressions/Capture/MemberExpressionsCapturer.cs
@@ -1,29 +1,27 @@
 ﻿namespace Fluent.Calculations.Primitives.Expressions.Capture;
-
 using Fluent.Calculations.Primitives.BaseTypes;
 using System.Linq;
 using System.Linq.Expressions;
 
 public class MemberExpressionsCapturer : ExpressionVisitor, IMemberExpressionsCapturer
 {
-    private readonly List<MemberExpression> capturedMemberExpressions = new List<MemberExpression>();
+    private readonly Dictionary<string, MemberExpression> capturedMemberExpressions = new Dictionary<string, MemberExpression>();
 
-    public List<MemberExpression> Capture<TExpressionResulValue>(Expression<Func<TExpressionResulValue>> lamdaExpression) where TExpressionResulValue : class, IValue
+    public MemberExpression[] Capture<TExpressionResulValue>(Expression<Func<TExpressionResulValue>> lamdaExpression) where TExpressionResulValue : class, IValue
     {
         Visit(lamdaExpression);
 
-        return capturedMemberExpressions;
+        return capturedMemberExpressions.Values.ToArray();
     }
 
     protected override Expression VisitMember(MemberExpression node) => base.VisitMember(CaptureIfNotExists(node));
 
     private MemberExpression CaptureIfNotExists(MemberExpression node)
     {
-        if (!capturedMemberExpressions.Any(WithSameName))
-            capturedMemberExpressions.Add(node);
+        string memberName = node.Member.Name;
+        if (!capturedMemberExpressions.ContainsKey(memberName))
+            capturedMemberExpressions.Add(memberName, node);
 
         return node;
-
-        bool WithSameName(MemberExpression memberExpression) => memberExpression.Member.Name.Equals(node.Member.Name);
     }
 }
