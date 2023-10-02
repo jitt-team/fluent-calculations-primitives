25fe6175d81d8bac43accd4ec1fcb23fac753d32
Edgars Pivovarenoks <edgarsp@jitt.lv>
2023-10-02 23:31:59 +0200

    cache tests, namespaces

--- src/Fluent.Calculations.Primitives.Tests/Expressions/MemberExpressionsCapturerTests.cs
+++ src/Fluent.Calculations.Primitives.Tests/Expressions/MemberExpressionsCapturerTests.cs
@@ -1,74 +1,72 @@
-﻿using Fluent.Calculations.Primitives.BaseTypes;
+﻿namespace Fluent.Calculations.Primitives.Tests.Expressions;
+using Fluent.Calculations.Primitives.BaseTypes;
 using Fluent.Calculations.Primitives.Expressions.Capture;
 using FluentAssertions;
 using System.Linq.Expressions;
 
-namespace Fluent.Calculations.Primitives.Tests.Expressions
+public class MemberExpressionsCapturerTests
 {
-    public class MemberExpressionsCapturerTests
+    [Fact]
+    public void LambdaWithMultipleMembers_IValue_AllAreCaptured()
     {
-        [Fact]
-        public void LambdaWithMultipleMembers_IValue_AllAreCaptured()
-        {
-            var capturer = new MemberExpressionsCapturer();
-            var testClass = new TestClass();
+        var capturer = new MemberExpressionsCapturer();
+        var testClass = new TestClass();
 
-            MemberExpression[] captureResult = capturer.Capture(() => testClass.TestFieldOne + testClass.TestFieldTwo + testClass.TestEvaluationOne + testClass.TestEvaluationTwo);
-            
-            string[] expectedMemberNames = new[] {
-                nameof(testClass.TestFieldOne),
-                nameof(testClass.TestFieldTwo),
-                nameof(testClass.TestEvaluationOne),
-                nameof(testClass.TestEvaluationTwo)
-            };
-
-            captureResult.Count().Should().Be(4);
-            captureResult.Should().Contain(e => expectedMemberNames.Contains(e.Member.Name));                   
-        }
-
-        [Fact]
-        public void LambdaWithDublicateMembers_IValue_UniqueAreCaptured()
-        {
-            var capturer = new MemberExpressionsCapturer();
-            var testClass = new TestClass();
-
-            MemberExpression[] captureResult = capturer.Capture(() => testClass.TestFieldOne + testClass.TestFieldOne + testClass.TestEvaluationOne + testClass.TestEvaluationOne);
-
-            string[] expectedMemberNames = new[] {
-                nameof(testClass.TestFieldOne),
-                nameof(testClass.TestEvaluationOne)
-            };
-
-            captureResult.Count().Should().Be(2);
-            captureResult.Should().Contain(e => expectedMemberNames.Contains(e.Member.Name));
-        }
-
-        [Fact]
-        public void LambdaWithUnsupportedMembers_UnsupportedAreIgnored()
-        {
-            var capturer = new MemberExpressionsCapturer();
-            var testClass = new TestClass();
-            decimal unsupportedValueType = 1;
-            MemberExpression[] captureResult = capturer.Capture(() => unsupportedValueType > 2 ? testClass.TestFieldOne : testClass.TestFieldTwo);
-
-            string[] expectedMemberNames = new[] {
-                nameof(testClass.TestFieldOne),
-                nameof(testClass.TestFieldTwo)
-            };
-
-            captureResult.Count().Should().Be(2);
-            captureResult.Should().Contain(e => expectedMemberNames.Contains(e.Member.Name));
-        }
-    }
-
-    public class TestClass
-    { 
-        public Number TestFieldOne = Number.Of(5.00m);
+        MemberExpression[] captureResult = capturer.Capture(() => testClass.TestFieldOne + testClass.TestFieldTwo + testClass.TestEvaluationOne + testClass.TestEvaluationTwo);
         
-        public Number TestFieldTwo = Number.Of(6.00m);
+        string[] expectedMemberNames = new[] {
+            nameof(testClass.TestFieldOne),
+            nameof(testClass.TestFieldTwo),
+            nameof(testClass.TestEvaluationOne),
+            nameof(testClass.TestEvaluationTwo)
+        };
 
-        public Number TestEvaluationOne => Number.Of(7.00m);
-
-        public Number TestEvaluationTwo => Number.Of(8.00m);
+        captureResult.Count().Should().Be(4);
+        captureResult.Should().Contain(e => expectedMemberNames.Contains(e.Member.Name));                   
     }
+
+    [Fact]
+    public void LambdaWithDublicateMembers_IValue_UniqueAreCaptured()
+    {
+        var capturer = new MemberExpressionsCapturer();
+        var testClass = new TestClass();
+
+        MemberExpression[] captureResult = capturer.Capture(() => testClass.TestFieldOne + testClass.TestFieldOne + testClass.TestEvaluationOne + testClass.TestEvaluationOne);
+
+        string[] expectedMemberNames = new[] {
+            nameof(testClass.TestFieldOne),
+            nameof(testClass.TestEvaluationOne)
+        };
+
+        captureResult.Count().Should().Be(2);
+        captureResult.Should().Contain(e => expectedMemberNames.Contains(e.Member.Name));
+    }
+
+    [Fact]
+    public void LambdaWithUnsupportedMembers_UnsupportedAreIgnored()
+    {
+        var capturer = new MemberExpressionsCapturer();
+        var testClass = new TestClass();
+        decimal unsupportedValueType = 1;
+        MemberExpression[] captureResult = capturer.Capture(() => unsupportedValueType > 2 ? testClass.TestFieldOne : testClass.TestFieldTwo);
+
+        string[] expectedMemberNames = new[] {
+            nameof(testClass.TestFieldOne),
+            nameof(testClass.TestFieldTwo)
+        };
+
+        captureResult.Count().Should().Be(2);
+        captureResult.Should().Contain(e => expectedMemberNames.Contains(e.Member.Name));
+    }
+}
+
+public class TestClass
+{ 
+    public Number TestFieldOne = Number.Of(5.00m);
+    
+    public Number TestFieldTwo = Number.Of(6.00m);
+
+    public Number TestEvaluationOne => Number.Of(7.00m);
+
+    public Number TestEvaluationTwo => Number.Of(8.00m);
 }
---
+++ src/Fluent.Calculations.Primitives.Tests/Expressions/ValuesCacheTests.cs
@@ -0,0 +1,38 @@
+﻿namespace Fluent.Calculations.Primitives.Tests.Expressions;
+using Fluent.Calculations.Primitives.BaseTypes;
+using Fluent.Calculations.Primitives.Expressions;
+using FluentAssertions;
+
+public class ValuesCacheTests
+{
+    [Fact]
+    public void ValueIsAddedToCache()
+    {
+        Dictionary<string, IValue> cacheStorage = new Dictionary<string, IValue>();
+        var cache = new ValuesCache(cacheStorage);
+        string testValueName = "TEST-VALUE";
+        cache.Add(Number.Of(1, testValueName));
+        cacheStorage.Values.Should().HaveCount(1);
+        cacheStorage.Values.Single().Name.Should().Be(testValueName);
+    }
+
+    [Fact]
+    public void GetValueByName_IsReturned()
+    {
+        var cache = new ValuesCache();
+        string testValueName = "TEST-VALUE";
+        cache.Add(Number.Of(1, testValueName));
+        var value = cache.GetByName(testValueName);
+        value.Name.Should().Be(testValueName);
+    }
+
+    [Fact]
+    public void GetValueByKey_IsReturned()
+    {
+        var cache = new ValuesCache();
+        string testValueName = "TEST-VALUE", key = "TEST-KEY";
+        cache.Add(key, Number.Of(1, testValueName));
+        var value = cache.GetByKey(key);
+        value.Name.Should().Be(testValueName);
+    }
+}
