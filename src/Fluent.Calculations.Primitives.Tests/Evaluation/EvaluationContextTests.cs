ad4dedb6b42e955095a36dbdd3ccfdde16a385de
Edgars Pivovarenoks <edgarsp@jitt.lv>
2023-10-04 16:16:54 +0300

    formatting

--- src/Fluent.Calculations.Primitives.Tests/Evaluation/EvaluationContextTests.cs
+++ src/Fluent.Calculations.Primitives.Tests/Evaluation/EvaluationContextTests.cs
@@ -1,18 +1,15 @@
-﻿using Fluent.Calculations.Primitives.BaseTypes;
+﻿namespace Fluent.Calculations.Primitives.Tests.Evaluation;
+using Fluent.Calculations.Primitives.BaseTypes;
 
-namespace Fluent.Calculations.Primitives.Tests.Evaluation
+public class EvaluationContextTests
 {
-    public class EvaluationContextTests
+    [Fact]
+    public void Test()
     {
+        EvaluationContext<Number> Calculation = new EvaluationContext<Number>();
 
-        [Fact]
-        public void Test()
-        {
-            EvaluationContext<Number> Calculation = new EvaluationContext<Number>();
+        Number NumberOne = Number.Of(5), NumberTwo = Number.Of(3);
 
-            Number NumberOne = Number.Of(5), NumberTwo = Number.Of(3);
-
-            Number result = Calculation.Evaluate(() => NumberOne * NumberTwo);
-        }
+        Number result = Calculation.Evaluate(() => NumberOne * NumberTwo);
     }
 }
