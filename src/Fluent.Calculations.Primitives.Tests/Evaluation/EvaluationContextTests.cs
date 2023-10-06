98c8f8582b2be22f7432e7d96e651b37130c309b
Edgars Pivovarenoks <edgarsp@jitt.lv>
2023-10-06 09:50:49 +0300

    more tests

--- src/Fluent.Calculations.Primitives.Tests/Evaluation/EvaluationContextTests.cs
+++ src/Fluent.Calculations.Primitives.Tests/Evaluation/EvaluationContextTests.cs
@@ -1,4 +1,4 @@
-﻿namespace Fluent.Calculations.Primitives.Tests.Evaluation;
+﻿
 using Fluent.Calculations.Primitives.BaseTypes;
 using Fluent.Calculations.Primitives.Expressions;
 using Fluent.Calculations.Primitives.Expressions.Capture;
@@ -6,60 +6,87 @@ using FluentAssertions;
 using Moq;
 using System.Linq.Expressions;
 
-public class EvaluationContextTests
+namespace Fluent.Calculations.Primitives.Tests.Evaluation
 {
-    private Mock<IValuesCache> _valuesCacheMock = new Mock<IValuesCache>(MockBehavior.Strict);
-    private Mock<IMemberExpressionValueCapturer> _memberCapturerMock = new Mock<IMemberExpressionValueCapturer>(MockBehavior.Strict);
-
-    [Fact]
-    public void Test()
+    public class EvaluationContextTests
     {
-        string
-            expectedCalculationName = "TEST-CALCULATION",
-            expectedNumberOneName = "NUMBER-1",
-            expectedNumberTwoName = "NUMBER-2";
-      
-        Number
-             NumberOne = Number.Of(5, Constants.NaN),
-             NumberTwo = Number.Of(3, Constants.NaN);
+        private Mock<IValuesCache> _valuesCacheMock = new Mock<IValuesCache>(MockBehavior.Strict);
+        private Mock<IMemberExpressionValueCapturer> _memberCapturerMock = new Mock<IMemberExpressionValueCapturer>(MockBehavior.Strict);
 
-        CapturedExpressionMembers capturedMembersMock = MockOnlyParameterCaptureResult(
-            NumberOne, expectedNumberOneName,
-            NumberTwo, expectedNumberTwoName);
+        [Fact]
+        public void Evaluation_Result_IsExpected()
+        {
+            var expected = new ExpectedTestValues();
+            Number result = RunParameterTestCase(expected);
+            result.Primitive.Should().Be(expected.PrimitiveValue);
+            result.Name.Should().Be(expected.CalculationName);
+        }
 
-        var calculation = MockAndBuildCalculation(expectedCalculationName, capturedMembersMock);
+        [Fact]
+        public void Evalueation_ExpressionArguments_AreExpected()
+        {
+            var expected = new ExpectedTestValues();
+            Number result = RunParameterTestCase(expected);
+            result.Expression.Arguments.Should().HaveCount(2);
+            result.Expression.Arguments.Should().Contain(a => a.Name.Equals(expected.NumberOneName));
+            result.Expression.Arguments.Should().Contain(a => a.Name.Equals(expected.NumberTwoName));
+        }
 
-        Number result = calculation.Evaluate(() => NumberOne * NumberTwo, expectedCalculationName);
+        [Fact]
+        public void Evaluation_EmptyCache_IsCached()
+        {
+            var inpuexpected = new ExpectedTestValues();
+            Number result = RunParameterTestCase(inpuexpected);
+            _valuesCacheMock.Verify(c => c.ContainsKey(inpuexpected.CalculationName), Times.Once());
+            _valuesCacheMock.Verify(c => c.Add(inpuexpected.CalculationName,
+                It.Is<IValue>(value => value.Name.Equals(inpuexpected.CalculationName) && value.Primitive.Equals(inpuexpected.PrimitiveValue))),
+                Times.Once());
+        }
 
-        result.Primitive.Should().Be(15m);
-        result.Name.Should().Be(expectedCalculationName);
-        result.Expression.Arguments.Should().HaveCount(2);
-        result.Expression.Arguments.Should().Contain(a => a.Name.Equals(expectedNumberOneName));
-        result.Expression.Arguments.Should().Contain(a => a.Name.Equals(expectedNumberTwoName));
-        _valuesCacheMock.Verify(c => c.ContainsKey(expectedCalculationName), Times.Once());
-        _valuesCacheMock.Verify(c => c.Add(expectedCalculationName,
-            It.Is<IValue>(value => value.Name.Equals(expectedCalculationName) && value.Primitive.Equals(15m))),
-            Times.Once());
-    }
+        public Number RunParameterTestCase(ExpectedTestValues expected)
+        {
+            Number
+                 NumberOne = Number.Of(5, Constants.NaN),
+                 NumberTwo = Number.Of(3, Constants.NaN);
 
-    private EvaluationContext<Number> MockAndBuildCalculation(string expectedCalculationName, CapturedExpressionMembers capturedMembersMock)
-    {
-        _valuesCacheMock.Setup(c => c.ContainsKey(expectedCalculationName)).Returns(false).Verifiable();
-        _valuesCacheMock.Setup(c => c.Add(expectedCalculationName, It.IsAny<IValue>())).Verifiable();
-        _memberCapturerMock.Setup(c => c.Capture(It.IsAny<Expression<Func<Number>>>())).Returns(capturedMembersMock).Verifiable();
+            CapturedExpressionMembers capturedMembersMock = MockParameterCaptureResult(
+                NumberOne, expected.NumberOneName,
+                NumberTwo, expected.NumberTwoName);
 
-        EvaluationContext<Number> calculation = new EvaluationContext<Number>(_valuesCacheMock.Object, _memberCapturerMock.Object);
-        return calculation;
-    }
+            EvaluationContext<Number> calculation = MockAndBuildCalculation(expected.CalculationName, capturedMembersMock);
 
-    private CapturedExpressionMembers MockOnlyParameterCaptureResult(IValue value1, string name1, IValue value2, string name2)
-    {
-        var parameterMemberers = new[] {
+            return calculation.Evaluate(() => NumberOne * NumberTwo, expected.CalculationName);
+        }
+
+        private EvaluationContext<Number> MockAndBuildCalculation(string expectedCalculationName, CapturedExpressionMembers capturedMembersMock)
+        {
+            _valuesCacheMock.Setup(c => c.ContainsKey(expectedCalculationName)).Returns(false).Verifiable();
+            _valuesCacheMock.Setup(c => c.Add(expectedCalculationName, It.IsAny<IValue>())).Verifiable();
+            _memberCapturerMock.Setup(c => c.Capture(It.IsAny<Expression<Func<Number>>>())).Returns(capturedMembersMock).Verifiable();
+
+            EvaluationContext<Number> calculation = new EvaluationContext<Number>(_valuesCacheMock.Object, _memberCapturerMock.Object);
+            return calculation;
+        }
+
+        private CapturedExpressionMembers MockParameterCaptureResult(IValue value1, string name1, IValue value2, string name2)
+        {
+            var parameterMemberers = new[] {
             new CapturedParameterMember(value1, name1),
             new CapturedParameterMember(value2, name2)
         };
-        var evaluationMembers = new CapturedEvaluationMember[0];
+            var evaluationMembers = new CapturedEvaluationMember[0];
 
-        return new CapturedExpressionMembers(parameterMemberers, evaluationMembers);
+            return new CapturedExpressionMembers(parameterMemberers, evaluationMembers);
+        }
+
+        public class ExpectedTestValues
+        {
+            public string
+                CalculationName = "TEST-CALCULATION",
+                NumberOneName = "NUMBER-1",
+                NumberTwoName = "NUMBER-2";
+
+            public decimal PrimitiveValue = 15m;
+        }
     }
 }
