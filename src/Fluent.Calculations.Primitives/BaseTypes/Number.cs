9e8572016610e8b6bd375ef0b9b832822222c2cc
Edgars Pivovarenoks <edgarsp@jitt.lv>
2023-10-08 09:10:11 +0300

    formatting fix

--- src/Fluent.Calculations.Primitives/BaseTypes/Number.cs
+++ src/Fluent.Calculations.Primitives/BaseTypes/Number.cs
@@ -16,13 +16,11 @@ public class Number : Value,
 
     public Number() : this(MakeValueArgs.Compose("Zero", new ExpressionNode($"0", ExpressionNodeType.Constant), 0)) { }
 
+    public Number(Number number) : base(number) { }
+
     public static implicit operator Number(int primitiveValue) => Number.Of(primitiveValue);
 
     public static implicit operator Number(decimal primitiveValue) => Number.Of(primitiveValue);
-
-    public Number(Number number) : base(number)
-    {
-    }
 
     public Number(MakeValueArgs createValueArgs) : base(createValueArgs)
     {
