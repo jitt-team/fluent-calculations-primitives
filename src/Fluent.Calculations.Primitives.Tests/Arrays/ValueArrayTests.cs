f8a85730fcc08811cc7b73b0359b10d9ee52f738
Edgars Pivovarenoks <edgarsp@jitt.lv>
2023-10-19 15:38:21 +0300

    wip

---
+++ src/Fluent.Calculations.Primitives.Tests/Arrays/ValueArrayTests.cs
@@ -0,0 +1,47 @@
+﻿using Fluent.Calculations.Primitives.BaseTypes;
+using System.Collections;
+
+namespace Fluent.Calculations.Primitives.Tests.Arrays;
+
+public class ValueArrayTests
+{
+    [Fact]
+    public void Test()
+    {
+
+
+    }
+
+    public static class Constant
+    {
+        public const string TestEvaluationName = "TEST-ARRAY-EVALUAITON-NAME";
+    }
+    public class Values<T> : IEnumerable<IValue>
+    {
+        private readonly List<IValue> numbers = new List<IValue>();
+
+        public void Add(IValue number) => numbers.Add(number);
+
+        public IEnumerator<IValue> GetEnumerator() => numbers.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => numbers.GetEnumerator();
+    }
+
+    //public class TestArrayCalcuation
+    //{
+    //    public static Number Return()
+    //    {
+    //        EvaluationOptions options = new() { AlwaysReadNamesFromExpressions = true };
+    //        EvaluationContext<Number> Calculation = new(options);
+
+    //        Number
+    //            NumberOne = Number.Of(2),
+    //            NumberTwo = Number.Of(3);
+
+    //        Numbers MultipleNumbers = new() { NumberOne, NumberTwo };
+
+    //        return Calculation.Evaluate(() => MultipleNumbers.Sum(), Constant.TestEvaluationName);
+    //    }
+
+    }
+}
