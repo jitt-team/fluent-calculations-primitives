08a66ea62a5256d8a7c7a16647aa1da63e3f04a1
Edgars Pivovarenoks <edgarsp@jitt.lv>
2023-11-09 21:31:02 +0200

    Update docusaurus.config.js

--- docusaurus/site/docusaurus.config.js
+++ docusaurus/site/docusaurus.config.js
@@ -44,14 +44,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/jitt-team/fluent-calculations-primitives/tree/main/docusaurus/site',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/jitt-team/fluent-calculations-primitives/tree/main/docusaurus/site',
         },
         theme: {
           customCss: './src/css/custom.css',
