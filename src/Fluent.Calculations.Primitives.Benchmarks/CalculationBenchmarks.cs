fdbf27a3f73fdb6f3f3b22b99bd261f8461ff78d
Edgars Pivovarenoks <edgarsp@jitt.lv>
2023-11-12 19:15:32 +0200

    Setting up BenchmarkDotNet

---
+++ src/Fluent.Calculations.Primitives.Benchmarks/CalculationBenchmarks.cs
@@ -0,0 +1,54 @@
+﻿using BenchmarkDotNet.Attributes;
+using Fluent.Calculations.Primitives.BaseTypes;
+
+namespace Fluent.Calculations.Primitives.Tests.Benchmarks
+{
+    [MemoryDiagnoser(false)]
+    public class CalculationBenchmarks
+    {
+        private readonly BasicAdditionWithContext additionContext;
+        private readonly BasicAdditionNative additionNative;
+
+        public CalculationBenchmarks()
+        {
+            additionContext = new BasicAdditionWithContext();
+            additionNative = new BasicAdditionNative();
+        }
+
+        [Benchmark]
+        public void Calculate_UsingContext()
+        {
+            Number result = additionContext.ToResult();
+        }
+
+        [Benchmark]
+        public void Calculate_Native()
+        {
+            decimal result = additionNative.Return();
+        }
+    }
+
+    public class BasicAdditionWithContext : EvaluationContext<Number>
+    {
+        public BasicAdditionWithContext() : base(new EvaluationOptions { AlwaysReadNamesFromExpressions = false }) { }
+
+        public Number
+            ValueOne = Number.Of(10),
+            ValueTwo = Number.Of(20);
+
+        Number Result => Evaluate(() => ValueOne + ValueTwo);
+
+        public override Number Return() => Result;
+    }
+
+    public class BasicAdditionNative
+    {
+        public decimal
+            ValueOne = 10,
+            ValueTwo = 20;
+
+        decimal Result => ValueOne + ValueTwo;
+
+        public decimal Return() => Result;
+    }
+}
---
+++ src/Fluent.Calculations.Primitives.Benchmarks/Program.cs
@@ -0,0 +1,6 @@
+﻿using BenchmarkDotNet.Reports;
+using BenchmarkDotNet.Running;
+using Fluent.Calculations.Primitives.Tests.Benchmarks;
+
+Summary summary = BenchmarkRunner.Run<CalculationBenchmarks>();
+Console.ReadLine();
