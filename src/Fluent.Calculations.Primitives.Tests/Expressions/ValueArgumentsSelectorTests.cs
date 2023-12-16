7af40a300d8fca249fc255657c51c6f690bb2d6f
Edgars Pivovarenoks <edgarsp@jitt.lv>
2023-12-16 13:56:15 +0200

    cleaner test

--- src/Fluent.Calculations.Primitives.Tests/Expressions/ValueArgumentsSelectorTests.cs
+++ src/Fluent.Calculations.Primitives.Tests/Expressions/ValueArgumentsSelectorTests.cs
@@ -1,41 +1,34 @@
-﻿using Fluent.Calculations.Primitives.BaseTypes;
+﻿namespace Fluent.Calculations.Primitives.Tests.Expressions;
+using Fluent.Calculations.Primitives.BaseTypes;
 using Fluent.Calculations.Primitives.Expressions;
 using FluentAssertions;
 
-namespace Fluent.Calculations.Primitives.Tests.Expressions
+public class ValueArgumentsSelectorTests
 {
-    public class ValueArgumentsSelectorTests
+    [Fact]
+    public void MultipleOperators_ReturnsOnlyArguments()
     {
+        ValueArgumentsSelector selector = new();
+        Number testResult = GetResult();
+        IValue[] arguments = selector.Select(testResult);
 
-
-        [Fact]
-        public void Test()
-        {
-            var selector = new ValueArgumentsSelector();
-            var testResult = GetPlainTestResult();
-            var arguments = selector.Select(testResult);
-
-            arguments.Length.Should().Be(2);
-        }
-
-        private Number GetPlainTestResult()
-        {
-            Number
-                one = Number.Of(1),
-                two = Number.Of(2),
-                three = Number.Of(3),
-                four = Number.Of(4);
-
-            return one + two * three - four * three + OtherCalculation(two);
-        }
-
-        private Number OtherCalculation(Number two)
-        {
-            Number
-                a = Number.Of(1),
-                b = Number.Of(2);
-
-            return a + b;
-        }
+        arguments.Length.Should().Be(4);
+        arguments[0].Name.Should().Be("one");
+        arguments[1].Name.Should().Be("two");
+        arguments[2].Name.Should().Be("three");
+        arguments[3].Name.Should().Be("SomeEvaluaton");
     }
+
+    private Number GetResult()
+    {
+        Number
+            one = Number.Of(1, nameof(one)),
+            two = Number.Of(2, nameof(two)),
+            three = Number.Of(3, nameof(three));
+
+        return one + two * three - three / MockEvaluationResult(two);
+    }
+
+    private Number MockEvaluationResult(Number two) => new(MakeValueArgs.Compose("SomeEvaluaton",
+            new ExpressionNode("10", ExpressionNodeType.Lambda), 10, ValueOriginType.Evaluation));
 }
