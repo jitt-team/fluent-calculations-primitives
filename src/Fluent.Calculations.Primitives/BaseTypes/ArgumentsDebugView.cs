a6dae6ac4fcb91891e0d0e5c842efbb74336cd2c
Edgars Pivovarenoks <edgarsp@jitt.lv>
2023-12-25 13:51:22 +0200

    adust xml

--- src/Fluent.Calculations.Primitives/BaseTypes/ArgumentsDebugView.cs
+++ src/Fluent.Calculations.Primitives/BaseTypes/ArgumentsDebugView.cs
@@ -1,8 +1,9 @@
 ﻿namespace Fluent.Calculations.Primitives.BaseTypes;
 using System.Diagnostics;
 
+#pragma warning disable CS1572 // XML comment has a param tag, but there is no parameter by that name
 /// <include file="Docs.xml" path='*/ArgumentsDebugView/class/*'/>
-/// <include file="Docs.xml" path='*/ArgumentsDebugView/ctor-arguments/*'/>
+/// #pragma warning restore CS1572 // XML comment has a param tag, but there is no parameter by that name
 public class ArgumentsDebugView(IArguments arguments)
 {
     private readonly IArguments arguments = arguments;
