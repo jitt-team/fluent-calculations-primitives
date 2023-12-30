455d7c3737aeb1d3dce39bfaa948758c00f5c198
Edgars Pivovarenoks <edgarsp@jitt.lv>
2023-12-30 21:17:37 +0200

    Update docusaurus.config.js

--- docusaurus/site/docusaurus.config.js
+++ docusaurus/site/docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://jitt-team.github.io',
+  url: 'https://docs.jitt.me/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/fluent-calculations-primitives/',
