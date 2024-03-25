3da91348291c5a1dd40aa05ab3c71a94083361e7
Edgars Pivovarenoks <edgarsp@jitt.lv>
2024-03-25 18:40:59 +0200

    typos

--- docusaurus/site/docusaurus.config.js
+++ docusaurus/site/docusaurus.config.js
@@ -8,8 +8,8 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Fluent Calcluations',
-  tagline: 'Create tracable calculations',
+  title: 'Fluent Calculations',
+  tagline: 'Create traceable calculations',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
